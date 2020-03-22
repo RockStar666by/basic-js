4c5483f852feb004efb4ac1f736bfa41acb2df91
RockStar666by <savchuk799@gmail.com>
2020-03-22 20:17:25 +0300

    add: solution for vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,64 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(reverse) {
+        this.reverse = reverse;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+
+        if (message === undefined || key === undefined) {
+            throw Error;
+        };
+
+        let result = '';
+        let alphabet = [];
+        let counter = 0;
+
+        for (let i = 65; i < 91; i++) {
+            alphabet.push(String.fromCharCode(i));
+        };
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        for (let j = 0; j < message.length; j++) {
+            if (alphabet.includes(message.charAt(j))) {
+                let pos = (alphabet.indexOf(message[j]) + alphabet.indexOf(key.charAt(counter % key.length))) % alphabet.length;
+                result += alphabet[pos];
+            } else {
+                counter = counter - 1;
+                result += message.charAt(j);
+            }
+            counter++;
+        };
+        if (this.reverse === false) { return result.split('').reverse().join('') } else { return result };
+    };
+
+    decrypt(message, key) {
+        if (message === undefined || key === undefined) {
+            throw Error;
+        }
+        let result = '';
+        let alphabet = [];
+        let counter = 0;
+
+        for (let i = 65; i < 91; i++) {
+            alphabet.push(String.fromCharCode(i));
+        }
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        for (let j = 0; j < message.length; j++) {
+            if (alphabet.includes(message.charAt(j))) {
+                let pos = ((alphabet.indexOf(message[j]) + alphabet.length) - alphabet.indexOf(key.charAt(counter % key.length))) % alphabet.length;
+                result += alphabet[pos];
+            } else {
+                counter = counter - 1;
+                result += message.charAt(j);
+            }
+            counter++;
+        };
+        if (this.reverse === false) { return result.split('').reverse().join('') } else { return result };
     }
 }
 
